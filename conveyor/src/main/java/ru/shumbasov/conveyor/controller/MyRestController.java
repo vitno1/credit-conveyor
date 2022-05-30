f7f754350f16fe8220dbdd411b94b4837786f016
student64 <student64@neoflex.ru>
2022-05-30 16:44:52 +0300

    Added service and controller

---
+++ conveyor/src/main/java/ru/shumbasov/conveyor/controller/MyRestController.java
@@ -0,0 +1,40 @@
+package ru.shumbasov.conveyor.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.shumbasov.conveyor.dto.CreditDTO;
+import ru.shumbasov.conveyor.dto.LoanApplicationRequestDTO;
+import ru.shumbasov.conveyor.dto.LoanOfferDTO;
+import ru.shumbasov.conveyor.service.OfferService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/conveyor")
+public class MyRestController {
+
+    private final OfferService offerService;
+
+
+    public MyRestController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+
+
+
+    @PostMapping("/offers")
+    public List<LoanOfferDTO> offers(@RequestBody LoanApplicationRequestDTO loanApplicationRequestDTO) {
+        offerService.setLoanApplicationRequestDTO(loanApplicationRequestDTO);
+        List<LoanOfferDTO> result = offerService.getOffers();
+        return result;
+    }
+
+    @PostMapping("/calculation")
+    public CreditDTO calculation() {
+        return null;
+    }
+}
---
+++ conveyor/src/main/java/ru/shumbasov/conveyor/service/OfferService.java
@@ -0,0 +1,56 @@
+package ru.shumbasov.conveyor.service;
+
+import org.springframework.stereotype.Service;
+import ru.shumbasov.conveyor.dto.LoanApplicationRequestDTO;
+import ru.shumbasov.conveyor.dto.LoanOfferDTO;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class OfferService {
+    //    private Long applicationId; //id кредита
+//    private BigDecimal requestedAmount; //запрашиваемая сумма
+//    private BigDecimal totalAmount; // общая сумма *
+//    private Integer term; //срок *
+//    private BigDecimal monthlyPayment; //ежемесячный платеж *
+//    private BigDecimal rate; //ставка * базовая ставка хардкодится из пропертей
+//    private Boolean isInsuranceEnabled; //включена ли страховка
+//    private Boolean isSalaryClient; //зп клиента норм?
+
+    private LoanApplicationRequestDTO loanApplicationRequestDTO;
+
+    public void setLoanApplicationRequestDTO(LoanApplicationRequestDTO loanApplicationRequestDTO) {
+        this.loanApplicationRequestDTO = loanApplicationRequestDTO;
+    }
+
+    private static long id = 1;
+
+    private void changeTotalAmount(){
+                BigDecimal totalAmount;
+        BigDecimal amount = loanApplicationRequestDTO.getAmount();
+        BigDecimal percent = new BigDecimal("1.1");
+        totalAmount = amount.multiply(percent);
+    }
+
+    private LoanOfferDTO getLoanOfferDTOWithIdAndRequestAmount(){
+        LoanOfferDTO loanOfferDTO = new LoanOfferDTO();
+        loanOfferDTO.setApplicationId(id++);
+        loanOfferDTO.setRequestedAmount(loanApplicationRequestDTO.getAmount());
+//        loanOfferDTO.setInsuranceEnabled(true);
+//        loanOfferDTO.setSalaryClient(true);
+
+
+        return loanOfferDTO;
+    }
+
+    public List<LoanOfferDTO> getOffers() {
+        List<LoanOfferDTO> result = new ArrayList<>(4);
+        LoanOfferDTO loanOfferDTO = getLoanOfferDTOWithIdAndRequestAmount();
+
+
+        return null;
+    }
+
+}
